fb0073f453a3593584ea90d1dd42a020dccbc15e
Wil Seely-Kirk <wil.seely-kirk@sap.com>
2017-09-18 16:14:36 -0700

    node module added

---
+++ node-minion/main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "default" {
+  count = "${var.node_count}"
+  name           = "node-minion-${count.index}"
+  machine_type   = "g1-small"
+  zone		 = "us-west1-a"
+  tags = ["terraform"]
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1704-zesty-v20170811"
+      }
+  }
+  network_interface {
+    network = "default"
+  }
+}
---
+++ node-minion/vars.tf
@@ -0,0 +1,3 @@
+variable "node_count" {
+  description = "The number of minion nodes you want to create"
+}
